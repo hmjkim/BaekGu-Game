3007eaa9a4f13a70efb5edd5e550be37c39f526a
yddobyb <139486122+yddobyb@users.noreply.github.com>
2024-11-23 16:10:42 -0800

    Create a get_user_choice function that takes input from the user for the options: '1: Direction', '2: Inventory', '3: Stat', and '4: Sleep' and create a basic game start structure within the game function

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 import random
 from make_board_each_level import *
+import time
 
 def make_character():
     return {
@@ -33,11 +34,61 @@ def make_character_location(grid):
     return first_location, prev_cell_content
 
 
+def get_user_choice(character):
+    types_input = ['1', '2', '3', '4']
+    user_wanted_input = input("which do you want to do? ['1: Direction','2: Inventory','3: Stat','4: Sleep']")
+    while user_wanted_input != '1':
+        if user_wanted_input == '2':
+            print(character['Inventory'])
+            user_wanted_input = input("which do you want to do again? ['1: Direction','2: Inventory','3: Stat','4: Sleep']")
+        if user_wanted_input == '3':
+            print("stats = ", character['Stat'])
+            print("skills = ", character['Skill'])
+            user_wanted_input = input("which do you want to do again? ['1: Direction','2: Inventory','3: Stat','4: Sleep']")
+        if user_wanted_input == '4':
+            print("go to sleep for 15sec")
+            time.sleep(15)
+            user_wanted_input = input("which do you want to do again? ['1: Direction','2: Inventory','3: Stat','4: Sleep']")
+        if user_wanted_input not in types_input:
+            print("invalid input")
+            user_wanted_input = input("which do you want to do again? ['1: Direction','2: Inventory','3: Stat','4: Sleep']")
+    if user_wanted_input == '1':
+        direction = ['w', 'a', 's', 'd', 'q']
+        full_direction = ['North', 'West', 'South', 'East', 'Quit']
+        for count, element in enumerate(direction):
+            print("%s : %s." % (full_direction[count], element), end=' ')
+        direction_input = input("\nenter the direction they wish to travel").lower()
+        return direction_input
+
+
 def game():
-    pass
+    """
+    Drive the game.
+    """
+    grid = make_board_lv1()
+    grid_lv2 = make_board_lv2()
+    grid_lv3 = make_board_lv3()
+
+    user_name = input("Hi, there! What's your name? : ")
+
+
+    first_location, prev_cell_content = make_character_location(grid)
+    character = make_character()
+
+    achieved_goal_lv1 = False
+    while not achieved_goal_lv1:
+        display_grid(grid)
+        direction = get_user_choice(character)
+        if direction == 'q':
+            print("end")
+            achieved_goal_lv1 = True
+
 
 
 def main():
+    """
+    Drive the program.
+    """
     game()
 
 
