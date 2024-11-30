32e8428af44cd484c58f46388e5657bedf063496
yddobyb <139486122+yddobyb@users.noreply.github.com>
2024-11-30 13:59:27 -0800

    Add docstrings and doctest

--- matching_direction_game.py
+++ matching_direction_game.py
@@ -2,10 +2,22 @@ import random
 import time
 
 
-# from game import make_character
+def check_character_level_matching_game(character: dict) -> int:
+    """
+    Check character's current level to determine the number of matches.
 
+    :param character: a dictionary of character including 'Level' key in 'Stat'.
+    :precondition: character must be a dictionary containing a nested dictionary under key 'Stat' and 'Level'
+    :postcondition: return the correct number of matches based on the character's level
+    :return: number of matches based on the character's level
 
-def check_character_level_matching_game(character):
+    >>> character1 = {'Stat': {'Level': 1}}
+    >>> check_character_level_matching_game(character1)
+    5
+    >>> character2 = {'Stat': {'Level': 3}}
+    >>> check_character_level_matching_game(character2)
+    9
+    """
     if character['Stat']['Level'] == 1:
         matching_count = 5
         return matching_count
@@ -17,26 +29,26 @@ def check_character_level_matching_game(character):
         return matching_count
 
 
-
 def play_game(level, character):
-    #explain
-    direction_list = ['A', 'D', 'S', 'W']
-    strings = random.choices(direction_list, k=level)
+    """
+    Play a direction game where the player memorize and input a sequence of directions ('A', 'D', 'S', 'W').
+
+    :param level: an integer representing the number of directions to memorize
+    :param character: a dictionary including the character's status, which includes 'Heart' as key in 'Stat'
+    :precondition: level must be a positive integer
+    :precondition: character must be a dictionary containing a nested dictionary under key 'Stat' with 'Heart' as a key
+    :postcondition: print the sequence of directions to memorize
+    :postcondition: get user input and convert to uppercase to compare with the sequence of directions
+    :postcondition: return a boolean indicating success or failure, and the updated character's heart
+    :return: a True or False through result of game and the updated character dictionary
+    """
+    strings = random.choices(['A', 'D', 'S', 'W'], k=level)
     print("memorize this direiton:")
     print(strings)
-
-    time.sleep(1)
-    print(5)
-    time.sleep(1)
-    print(4)
-    time.sleep(1)
-    print(3)
-    time.sleep(1)
-    print(2)
-    time.sleep(1)
-    print(1)
+    for i in range(5):
+        time.sleep(1)
+        print(5-i)
     print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
-
     print("Enter the answer")
     user_inputs = [input(f"{i+1} direction: ") for i in range(len(strings))]
     upper_inputs = [i.upper() for i in user_inputs]
@@ -51,13 +63,13 @@ def play_game(level, character):
         return False, character
 
 
-# def main():
-#     character = make_character()
-#     level_matching_game = check_character_level_matching_game(character)
-#     check, character = play_game(level_matching_game, character)
-#     print(check, character)
-#
-#
-# if __name__ == "__main__":
-#     main()
+def main():
+    character = {'Stat': {'Level': 1, 'Heart': 5}}
+    level_matching_game = check_character_level_matching_game(character)
+    check, character = play_game(level_matching_game, character)
+    print(check, character)
+
+
+if __name__ == "__main__":
+    main()
 
