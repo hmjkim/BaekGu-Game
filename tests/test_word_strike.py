bb6b82117bd88434ba995bded0a629ad6ae29119
yddobyb <139486122+yddobyb@users.noreply.github.com>
2024-12-02 11:41:17 -0800

    Add unit tests to cover a wider range of scenarios

--- tests/test_word_strike.py
+++ tests/test_word_strike.py
@@ -21,6 +21,25 @@ class Test(TestCase):
         expected = '̶p ̶y ̶t ̶h ̶o ̶n'
         self.assertEqual(actual, expected)
 
+    def test_word_strike_punctuation(self):
+        word = '!'
+        actual = word_strike(word)
+        expected = '̶!'
+        self.assertEqual(actual, expected)
+
+    def test_word_strike_mixed_punctuation(self):
+        word = 'dog!'
+        actual = word_strike(word)
+        expected = '̶d ̶o ̶g ̶!'
+        self.assertEqual(actual, expected)
+
+    def test_word_strike_mixed_number(self):
+        word = 'dog!123'
+        actual = word_strike(word)
+        expected = '̶d ̶o ̶g ̶! ̶1 ̶2 ̶3'
+        self.assertEqual(actual, expected)
+
+
 
 
 
