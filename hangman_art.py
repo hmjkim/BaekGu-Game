cfeb12f93060e1f99e8c905c7f172f8a3fceaa5b
yddobyb <139486122+yddobyb@users.noreply.github.com>
2024-12-03 01:41:34 -0800

    Import warnings.filterwarnings("ignore") for removing SyntaxWarning

--- hangman_art.py
+++ hangman_art.py
@@ -1,4 +1,26 @@
+import warnings
+warnings.filterwarnings("ignore")
+
+
 def stage():
+    """
+    Create a list representing stages of a hangman game as visual.
+
+    :postcondition: return a list containing art strings 
+    :return: list of art strings representing hangman stages.
+    
+    >>> stages = stage()
+    >>> print(stages[0])
+    <BLANKLINE>
+      +---+
+      |   |
+      O   |
+     /|\  |
+     / \  |
+          |
+    =========
+    <BLANKLINE>
+    """
     stages = ['''
   +---+
   |   |
