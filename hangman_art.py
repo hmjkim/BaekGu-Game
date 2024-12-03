cfa88c7b1175915439598b04be97a3311839af23
yddobyb <139486122+yddobyb@users.noreply.github.com>
2024-12-03 01:42:20 -0800

    Add docstring and doctest

--- hangman_art.py
+++ hangman_art.py
@@ -7,7 +7,7 @@ def stage():
     Create a list representing stages of a hangman game as visual.
 
     :postcondition: return a list containing art strings 
-    :return: list of art strings representing hangman stages.
+    :return: list of art strings representing hangman stages
     
     >>> stages = stage()
     >>> print(stages[0])
