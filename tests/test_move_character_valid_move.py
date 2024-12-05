fac463594232b1a03869bea9d7c26268c9eb1e7a
Heather Kim <heathermjkim@gmail.com>
2024-12-05 00:01:54 -0800

    Remove unused variable in unittest

--- tests/test_move_character_valid_move.py
+++ tests/test_move_character_valid_move.py
@@ -19,10 +19,8 @@ class TestMoveCharacterValidMove(TestCase):
         direction = 'd'
         prev_cell_content = '.'
         character = {"Stat": {"Hunger": 10}}
-
-        new_position, new_prev_cell_content, updated_character, valid_check = move_character_valid_move(
+        move_character_valid_move(
             grid, position, direction, prev_cell_content, character)
-
         print_result = mock_output.getvalue()
         expected_result = "You moved one step right. Everything seems quiet.\n"
         self.assertEqual(print_result, expected_result)
